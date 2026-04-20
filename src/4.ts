6f429704799bcb132aa863073e4b0d0e2040feab
irynastefanyshyn9@gmail.comgit config --global user.nameirynastefanyshyn9-beep <irynastefanyshyn@Irynas-MacBook-Pro.local>
2026-04-20 10:36:20 -0500

    add files

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
 
-house.comeIn(person);
+  getKey(): Key {
+    return this.key;
+  }
+}
 
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person came in. Welcome home!');
+    } else {
+      console.log('The door is locked. You cannot enter.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is now open.');
+    } else {
+      console.log('Wrong key! The door remains closed.');
+    }
+  }
+}
+
+
+const key = new Key(); 
+const house = new MyHouse(key); 
+const person = new Person(key); 
+
+house.openDoor(person.getKey()); 
+house.comeIn(person); 
 
 export {};
